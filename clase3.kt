51c788820aab4eaf666cf8bcf33cb9afb0e3c5d3
Julián Swerdlin <julianswer36@gmail.com>
2022-04-26 20:40:38 -0300

    Uploading file from 3rd class. 26-04-22.

---
+++ clase3.kt
@@ -0,0 +1,38 @@
+fun main(args: Array<String>) {
+
+    val age = 14
+    val altura = 140
+
+    if(age >= 16 && altura >= 120) {
+        println("fenomeno, podes subir")
+    } else {
+        println("Lo lamento, no cumplis con los requisitos minimos")
+    }
+
+        /* PERSONAL CHALLENGE val personas = [
+        {
+            name: "pepe",
+            altura: 120,
+            age: 15
+        },
+        {
+            name: "maria",
+            altura: 160,
+            age: 19
+        },
+        {
+            name: "lomabardo",
+            altura: 100,
+            age: 12
+        },
+    ]
+
+    for (persona in personas) {
+        if(persona.age >= 16 && persona.altura >= 120) {
+            println("que bien, $persona.name puede pasar")
+        } else {
+            println("lamentablemente $persona.name no va a poder entrar")
+        }
+    }
+    */
+}
